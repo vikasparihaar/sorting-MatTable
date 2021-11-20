47ff67a0fe179ef4d38b44145c92adaffd26bcdf
vikas <vikasparihar864@gmail.com>
2021-11-20 16:03:13 +0530

    sorted done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import studentData from './student.json';
+
+export interface Student {
+ fil: string;
+  name: string;
+  produkt: string;
+  bestand: string;
+  nettoneuakquistion: string;
+  aktueller:string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'layout2';
+  datasource:Student[]=studentData;
+  displayedColumns: string[] = ['fil', 'name', 'produkt', 'bestand','nettoneuakquistion','aktueller'];
+  dataSource = new MatTableDataSource(studentData);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
